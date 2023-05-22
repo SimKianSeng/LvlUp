e59f1363a5b8b8a317fc86cce638e6a2690448b9
SimKianSeng <simkianseng28@gmail.com>
2023-05-22 22:59:16 +0800

    authentication inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
+import 'package:lvlup/widget_tree.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+// void main() {
+//   runApp(MaterialApp(
+//     home: Home(),
+//   ));
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const WidgetTree(),
+    );
+  }
 }
 
 class Home extends StatefulWidget {
@@ -21,7 +44,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.purple[200],
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Home",
           style: TextStyle(
             fontSize: 18.0,
@@ -40,7 +63,7 @@ class _HomeState extends State<Home> {
             counter++;
           });
         },
-        child: Row(
+        child: const Row(
           children: <Widget>[
             Text("Add"),
             Icon(Icons.add),
