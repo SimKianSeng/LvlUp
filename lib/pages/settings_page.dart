da7ff39127e29235c7a62e57b27c7f24cd3157a6
SimKianSeng <simkianseng28@gmail.com>
2023-05-28 20:53:41 +0800

    Email validation

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -2,48 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:lvlup/constants.dart';
 import '../services/auth.dart';
 
-  Future<void> signOut() async {
-    await Auth().signOut();
-  }
+Future<void> signOut() async {
+  await Auth().signOut();
+}
 
-  Widget _signOutButton(BuildContext context) {
-    return ElevatedButton(
+Widget _signOutButton(BuildContext context) {
+  return ElevatedButton(
+    style: ButtonStyle(
+        shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)))),
+    onPressed: () async {
+      signOut();
+      Navigator.pop(context);
+    },
+    child: const Text('Sign Out'),
+  );
+}
+
+Widget _resetPasswordButton() {
+  return SizedBox(
+    width: 250.0,
+    child: ElevatedButton(
       style: ButtonStyle(
-        shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)))
-      ),
-      onPressed: () async {
-        signOut();
-        Navigator.pop(context);
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0)))),
+      onPressed: () {
+        print("Reset Passsword");
       },
-      child: const Text('Sign Out'),
-    );
-  }
+      child: const Text('Reset Password'),
+    ),
+  );
+}
 
-  Widget _resetPasswordButton() {
-    return SizedBox(
-      width: 250.0,
-      child: ElevatedButton(
-        style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)))),
-          onPressed: () {print("Reset Passsword");},
-          child: const Text('Reset Password'),
-        ),
-    );
-  }
-
-  Widget _deleteAccountButton() {
-    return SizedBox(
-      width: 250.0,
-      child: ElevatedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)))),
-          onPressed: () {print("Delete account");},
-          child: const Text('Delete Account'),
-        ),
-      );
-  }
+Widget _deleteAccountButton() {
+  return SizedBox(
+    width: 250.0,
+    child: ElevatedButton(
+      style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0)))),
+      onPressed: () {
+        print("Delete account");
+      },
+      child: const Text('Delete Account'),
+    ),
+  );
+}
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -63,7 +69,10 @@ class Settings extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             _resetPasswordButton(),
-            SizedBox(width: double.infinity, height: 50.0,),
+            SizedBox(
+              width: double.infinity,
+              height: 50.0,
+            ),
             _deleteAccountButton(),
           ],
         ),
