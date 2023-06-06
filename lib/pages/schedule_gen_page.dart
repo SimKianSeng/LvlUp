2ea688593ed5454b5e0c1a0de2564773ef2ccae4
Sebastian Tay <taysebastian95@gmail.com>
2023-06-07 00:31:56 +0800

    added navig bar for schedule page; testing gitignore

--- lib/pages/schedule_gen_page.dart
+++ lib/pages/schedule_gen_page.dart
@@ -8,13 +8,44 @@ class ScheduleGen extends StatefulWidget {
   State<ScheduleGen> createState() => _ScheduleGenState();
 }
 
+Widget _GeneratorButton() {
+  return ElevatedButton(
+    style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)))),
+    onPressed: () {
+
+    },
+    child: const Text("Generator"),);
+}
+
 class _ScheduleGenState extends State<ScheduleGen> {
+
+  Widget _editButton() {
+    return ElevatedButton(
+    style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)))),
+    onPressed: () {
+
+    },
+    child: const Text("Edit"),);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Schedule")),
       body: Container(
         decoration: bgColour,
+      ),
+      bottomNavigationBar: NavigationBar(
+        height: 50.0,
+        backgroundColor: Colors.white30,
+        destinations: [
+          _GeneratorButton(),
+          _editButton(),
+        ],
       ),
     );
   }
