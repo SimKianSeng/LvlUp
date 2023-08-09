5fe770c6b83a001f64fc810fb4fcd6aa4b3e17b1
Sebastian Tay <taysebastian95@gmail.com>
2023-08-09 17:16:18 +0800

    Code Refactoring

--- lib/pages/schedule/schedule_input.dart
+++ lib/pages/schedule/schedule_input.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:lvlup/constants.dart';
 import 'package:lvlup/models/app_user.dart';
 import 'package:lvlup/models/session.dart';
-import 'package:lvlup/widgets/module_row.dart';
 import 'package:lvlup/services/generator.dart';
 import 'package:time_planner/time_planner.dart';
 
@@ -258,22 +257,38 @@ class _ScheduleInputState extends State<ScheduleInput>{
                 });
             },
             onStepContinue: () {
-              //TODO Update database and generator, move on afterwards
               bool isLastStep = (currentStep == steps().length - 1);
 
-              if (isLastStep) {
-                //Save generator new settings
-                _generator.saveToDatabase(user);
+              //TODO ensure that user do not leave the current page empty for step 0 and 1, which are the only steps apart from the last step
+              //TODO module list not working
+              if (!isLastStep) {
+                bool hasNoInputs = currentStep == 0
+                ? _modules.isEmpty //Check that module list contains at least 1 module
+                : sessions.isEmpty;//Check that free Periods list has at least 1 session in it
 
-                //Exit input page
-                Navigator.pop(context);
-              } else {
-                //Move to the next step
-                setState(() {
-                  currentStep++;
-                });
+                if (hasNoInputs) {
+                  const message = SnackBar(
+                    content: Text('Please fill in the inputs for the current step!'),
+                  );
+                  
+                  ScaffoldMessenger.of(context).showSnackBar(message);
+                } else {
+                  //Move to the next step
+                  setState(() {
+                    currentStep++;
+                  });
+                }
+
+                return;
               }
-            },
+
+              //Last step does not require us to check for any inputs, continue callback should save the new inputs and exit the page
+              //Save generator new settings
+              _generator.saveToDatabase(user);
+
+              //Exit input page
+              Navigator.pop(context);
+           },
             onStepTapped: (newStep) => setState(() {
               currentStep = newStep;
             }),
@@ -283,5 +298,78 @@ class _ScheduleInputState extends State<ScheduleInput>{
           )
         )
       );
+  }
+}
+
+
+class ModuleRow extends StatefulWidget {
+  final int index;
+  final Generator generator = Generator();
+  final String originalInput;
+  final FocusNode? focusNode;
+
+  ModuleRow({required this.index, required this.focusNode, this.originalInput = '', super.key});
+
+  @override
+  State<ModuleRow> createState() => _ModuleRowState();
+}
+
+class _ModuleRowState extends State<ModuleRow> {
+  String module = '';
+
+  bool hasNoInput() {
+    return module == '' && widget.originalInput == '';
+  }
+
+ void updateModule(String module) {
+    if (widget.generator.alreadyInput(module)) {
+      //module has been input, do not update
+      const message = SnackBar(
+        content: Text('Module has been entered previously'),
+      );
+      
+      ScaffoldMessenger.of(context).showSnackBar(message);
+    } else if (module != '' && this.module != module) {
+      //module has not been previously input, update
+      this.module = module;
+    }
+    
+    widget.generator.updateModule(module, widget.index);  
+  }
+
+  //TODO removing the module input
+  // Widget _removeModuleInput() {
+  //   return GestureDetector(
+  //     child: const Icon(Icons.menu, color: Colors.black,),
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(width: widget.index == 1 ? 68.0 : 135.0),
+          widget.index == 1 ? const Text('Module 1 (Weakest):') : Text('Module ${widget.index}:'),
+          const SizedBox(width: 25.0,),
+          SizedBox(
+            width: 100.0,
+            height: 35.0,
+            //we can use textformfield to accomplish the task of auto populating the input
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              focusNode: widget.focusNode,
+              autofocus: true,
+              onChanged: (value) => updateModule(value),
+              decoration: customTextField(),
+              initialValue: widget.originalInput,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
